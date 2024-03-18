d5d89a9fb934a816998b90a34c29c32c7e55231a
Анна Романчук <romanchukanna05@icloud.com>
2024-03-18 15:11:23 +0200

    subarray fusion method

--- one.java
+++ one.java
@@ -7,6 +7,7 @@ public class one {
 
        
         mergeSort(b, 0, b.length - 1);
+        System.out.println(Arrays.toString(b));
 
         int median = Median(b);
         System.out.println("Медіана: ");
@@ -24,6 +25,41 @@ public class one {
             mergeSort(arr, middle + 1, right);
             merge(arr, left, middle, right);
         }
+    }
+    public static void merge(int[] arr, int left, int middle, int right) {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        for (int i = 0; i < n1; ++i) {
+            L[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; ++j) {
+            R[j] = arr[middle + 1 + j];
+        }
+        int i = 0, j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
         public static int Median(int[] arr) {
             int n = arr.length;
             if (n % 2 == 0) {
