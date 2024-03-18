3cd5b440d0ec9e01de673a85fef3b8d40d282916
Анна Романчук <romanchukanna05@icloud.com>
2024-03-18 14:31:40 +0200

    added toBinar and for result

--- App.java
+++ App.java
@@ -1,11 +1,18 @@
 public class App {
-    
+
     public static void main(String[] args) {
 
-        
-            int a = 224;
-            boolean minus = a < 0;
-            int n = minus ? -a: a;
-            
+        int a = 224;
+        boolean minus = a < 0;
+        int n = minus ? -a : a;
+        String binarResult = toBinar(n, 32);
+
+        System.out.println(a);
+        System.out.println(binarResult);
+    }
+
+    public static String toBinar(int n, int b) {
+        String binar = "";
+        return binar.toString();
     }
 }
