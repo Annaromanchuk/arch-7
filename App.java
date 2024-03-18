fb558c3ac8e4d8eab49821628008320338246f4a
Анна Романчук <romanchukanna05@icloud.com>
2024-03-18 14:22:05 +0200

    add a and negative numbers

---
+++ App.java
@@ -0,0 +1,11 @@
+public class App {
+    
+    public static void main(String[] args) {
+
+        
+            int a = 224;
+            boolean minus = a < 0;
+            int n = minus ? -a: a;
+            
+    }
+}
