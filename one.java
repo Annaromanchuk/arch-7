e7dac359d53ee7d5d97d50f4cdfa9bcb7b206955
Анна Романчук <romanchukanna05@icloud.com>
2024-03-18 15:06:38 +0200

    added median and average value

--- one.java
+++ one.java
@@ -8,7 +8,14 @@ public class one {
        
         mergeSort(b, 0, b.length - 1);
 
-       
+        int median = Median(b);
+        System.out.println("Медіана: ");
+        System.out.println(median);
+
+        int average = Average(b);
+        System.out.println("Середнє значення: ");
+        System.out.println(average);
+    }
 
     public static void mergeSort(int[] arr, int left, int right) {
         if (left < right) {
@@ -17,6 +24,21 @@ public class one {
             mergeSort(arr, middle + 1, right);
             merge(arr, left, middle, right);
         }
+        public static int Median(int[] arr) {
+            int n = arr.length;
+            if (n % 2 == 0) {
+                return (arr[n / 2] + arr[n / 2 - 1]) / 2;
+            } else {
+                return arr[n / 2];
+            }
+        }
+    
+        public static int Average(int[] arr) {
+            int sum = 0;
+            for (int num : arr) {
+                sum += num;
+            }
+            return sum / arr.length;
     }
 }
 
