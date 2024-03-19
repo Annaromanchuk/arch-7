c3f46a39c5ab95d004c820b5e8e5ff065e74c102
Анна Романчук <romanchukanna05@icloud.com>
2024-03-19 22:32:59 +0200

    added converting a number to a binary number

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int[] numbers = new int[10000];
+        int c = 0;
+        while (scanner.hasNextInt()) {
+            int a = scanner.nextInt();
+            boolean minus = a < 0;
+            int n = minus ? -a : a;
+            String binarResult = toBinar(n, 32);
+            System.out.println(a);
+            System.out.println(binarResult);
+        }
+
     }
 }
