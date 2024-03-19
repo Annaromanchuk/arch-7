5b9188d5d950fe3dca3872a09cc631365c2f0ee3
Анна Романчук <romanchukanna05@icloud.com>
2024-03-19 22:59:15 +0200

    delted scanner

--- Main.java
+++ Main.java
@@ -1,18 +1,15 @@
 import java.util.Arrays;
-import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int[] numbers = new int[10000];
-        int c = 0;
-        while (scanner.hasNextInt()) {
-            int a = scanner.nextInt();
+        
+        int a = 224;
             boolean minus = a < 0;
             int n = minus ? -a : a;
             String binarResult = toBinar(n, 32);
             System.out.println(a);
             System.out.println(binarResult);
-        }
+        
         int[] b = {0, 4, 2, 5, 9};
         mergeSort(b, 0, b.length - 1);
 //* треба ввести будь-який знак,
@@ -27,7 +24,7 @@ public class Main {
         double average = Average(b);
         System.out.println("Середнє значення:");
         System.out.println(average);
-        scanner.close();
+    
     }
     public static String toBinar(int n, int b) {
         String binar = "";
